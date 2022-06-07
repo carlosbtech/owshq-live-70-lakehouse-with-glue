83f038c8346bb227c1d3c64870c2c2d2beab443f
Carlos Barbosa <carlosbarbosa@Carloss-MacBook-Pro.local>
2022-06-07 18:44:37 -0300

    fix: improvements

--- terraform/glue.tf
+++ terraform/glue.tf
@@ -1,15 +1,16 @@
 resource "aws_glue_job" "glue_job" {
-  name         = "glue_script"
-  role_arn     = aws_iam_role.glue_job.arn
-  glue_version = "3.0"
-  worker_type = "Standard"
+  name              = "glue_script"
+  role_arn          = aws_iam_role.glue_job.arn
+  glue_version      = "3.0"
+  worker_type       = "Standard"
   number_of_workers = 2
-  timeout = 5
+  timeout           = 5
 
   command {
     script_location = "s3://${local.glue_bucket}/glue-etl.py"
     python_version  = "3"
   }
+
   default_arguments = {
     "--extra-jars" = "s3://owshq-aws-glue-scripts-777696598735/jars/delta-core_2.12-1.0.0.jar"
     "--conf spark.delta.logStore.class" = "org.apache.spark.sql.delta.storage.S3SingleDriverLogStore"
