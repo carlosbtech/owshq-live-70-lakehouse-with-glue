19ab4915a347964060680c886ba6068a4b462f74
Carlos Barbosa <jccbarbosa98@icloud.com>
2022-06-07 22:25:54 -0300

    feat: add new workflow

--- app/glue-etl.py
+++ app/glue-etl.py
@@ -1,19 +1,31 @@
 from pyspark.sql import SparkSession
+from delta.tables import DeltaTable
 
 class FakeDataFrame():
     
-    def __init__(self, spark=SparkSession.builder.getOrCreate()) -> SparkSession:
+    def __init__(self,
+                spark=SparkSession.builder.getOrCreate(), 
+                destination_path="s3://owshq-bronze-777696598735/fake/"
+                ) -> SparkSession:
+        
         self.spark = spark
+        self.destination_path = destination_path
 
     def fake_frame(self):
         return self.spark.range(0,1000)
     
-    def write_fake_frame(self, df, destination_path):
-        return df.write.mode("overwrite").format("delta").save(destination_path)
+    def write_fake_frame(self, df):
+        return (
+            df.coalesce(1).write.mode("overwrite").format("delta").save(self.destination_path)
+        )
+
+    def show_history_table(self):
+        return DeltaTable.forPath(self.spark, self.destination_path).history().show(truncate=False, vertical=True)
 
 f = FakeDataFrame()
 
 df = f.fake_frame()
 df.show(vertical=True)
 
-f.write_fake_frame(df, destination_path="s3://owshq-bronze-777696598735/fake/")
+f.write_fake_frame(df)
+f.show_history_table()
