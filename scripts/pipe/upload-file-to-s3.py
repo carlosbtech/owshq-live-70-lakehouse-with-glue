c9d9878a0b999e60807aeaa1f7bb2a63be0bd19c
Carlos Barbosa <carlosbarbosa@Carloss-MacBook-Pro.local>
2022-06-07 21:29:03 -0300

    feat: add cd

--- scripts/pipe/upload-file-to-s3.py
+++ scripts/pipe/upload-file-to-s3.py
@@ -2,7 +2,7 @@ import boto3
 import sys
 
 def main():
-    if (len(sys.argv), 6):
+    if (len(sys.argv) > 6):
         print ('Error: Required 5 arguments.')
         # Checks for 6 because the script path is in position 0. So len is 6
         # for 5 arguments.
